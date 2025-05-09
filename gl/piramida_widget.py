7deda89d89ad3ca7f6b8a6f5b1f3e7414ee52bfb
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-05-09 11:39:47 +0300

    add load function for piramida widget (.obj format)

--- gl/piramida_widget.py
+++ gl/piramida_widget.py
@@ -44,6 +44,34 @@ class PyraWidget(QOpenGLWidget):
         self.distance = -5.0
         self.last_pos = QPoint()
         self.color = (1.0, 1.0, 1.0)
+        self.vertices = []
+        self.normals = []
+        self.faces = []
+
+    def load_obj(self, filepath: str):
+        self.vertices.clear()
+        self.normals.clear()
+        self.faces.clear()
+        with open(filepath, "r") as f:
+            for line in f:
+                parts = line.strip().split()
+                if not parts:
+                    continue
+                if parts[0] == "v":
+                    _, x, y, z = parts
+                    self.vertices.append((float(x), float(y), float(z)))
+                elif parts[0] == "vn":
+                    _, x, y, z = parts
+                    self.normals.append((float(x), float(y), float(z)))
+                elif parts[0] == "f":
+                    face = []
+                    for v in parts[1:]:
+                        vals = v.split('//')
+                        vi = int(vals[0]) - 1
+                        ni = int(vals[1]) - 1 if len(vals) > 1 and vals[1] else None
+                        face.append((vi, ni))
+                    self.faces.append(face)
+        self.update()
 
     def set_color(self, qcolor: QColor):
         r = qcolor.redF()
@@ -72,11 +100,14 @@ class PyraWidget(QOpenGLWidget):
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
-        glTranslatef(0, 0, self.distance)
+        glTranslatef(0, -self.base_size * 0.5, self.distance)
         glRotatef(self.x_rot / 16.0, 1, 0, 0)
         glRotatef(self.y_rot / 16.0, 0, 1, 0)
         glColor3f(*self.color)
-        self._draw_pyramid()
+        if self.faces:
+            self._draw_model()
+        else:
+            self._draw_pyramid()
 
     def _draw_pyramid(self):
         s = self.base_size
@@ -90,6 +121,31 @@ class PyraWidget(QOpenGLWidget):
         glVertex3f(-s, s, 0)
         glEnd()
 
+        glBegin(GL_TRIANGLES)
+        normals = [(0, -1, 1), (1, 0, 1), (0, 1, 1), (-1, 0, 1)]
+        verts = [(-s, -s), (s, -s), (s, s), (-s, s)]
+        for i in range(4):
+            nx, ny, nz = normals[i]
+            glNormal3f(nx, ny, nz)
+            x1, y1 = verts[i]
+            x2, y2 = verts[(i + 1) % 4]
+            glVertex3f(x1, y1, 0)
+            glVertex3f(x2, y2, 0)
+            glVertex3f(0, 0, h)
+        glEnd()
+
+    def _draw_model(self):
+        s = self.base_size
+        h = self.height
+        glBegin(GL_TRIANGLES)
+        for face in self.faces:
+            for vi, ni in face:
+                if ni is not None and ni < len(self.normals):
+                    nx, ny, nz = self.normals[ni]
+                    glNormal3f(nx, ny, nz)
+                x, y, z = self.vertices[vi]
+                glVertex3f(x, y, z)
+        glEnd()
         glBegin(GL_TRIANGLES)
         normals = [(0, -1, 1), (1, 0, 1), (0, 1, 1), (-1, 0, 1)]
         verts = [(-s, -s), (s, -s), (s, s), (-s, s)]
@@ -117,7 +173,7 @@ class PyraWidget(QOpenGLWidget):
 
     def wheelEvent(self, event):
         delta = event.angleDelta().y()
-        self.distance += -delta / 240.0
+        self.distance += delta / 240.0
         self.update()
 
     def reset_view(self):
